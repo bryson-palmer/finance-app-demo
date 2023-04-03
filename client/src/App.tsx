39c3638f3e62b668c00192f3b63cf88980f3589e
Bryson Palmer <brysonpalmer@gmail.com>
2023-04-03 11:53:47 -0700

    Formatting files to replace all double quotes to single quotes.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,25 @@
-import { useMemo } from "react"
+import { useMemo } from 'react'
 
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import { Box, CssBaseline, ThemeProvider } from "@mui/material"
-import { createTheme } from "@mui/material/styles"
+import { Box, CssBaseline, ThemeProvider } from '@mui/material'
+import { createTheme } from '@mui/material/styles'
 
-import { themeSettings } from "@/theme"
-import Navbar from "@/scenes/navbar"
+import { themeSettings } from '@/theme'
+import Navbar from '@/scenes/navbar'
 
 const App = () => {
   const theme = useMemo(() => createTheme(themeSettings), [])
   return (
-    <div className="app">
+    <div className='app'>
       <BrowserRouter>
         <ThemeProvider theme={theme}>
           <CssBaseline />
-          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+          <Box width='100%' height='100%' padding='1rem 2rem 4rem 2rem'>
             <Navbar />
             <Routes>
-              <Route path="/" element={<div>Dashboard Page</div>} />
-              <Route path="/predictions" element={<div>Predictions Page</div>} />
+              <Route path='/' element={<div>Dashboard Page</div>} />
+              <Route path='/predictions' element={<div>Predictions Page</div>} />
             </Routes>
           </Box>
         </ThemeProvider>
