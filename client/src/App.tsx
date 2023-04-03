c0532953840cceb509d4c242e56afe9f2cdcece8
Bryson Palmer <brysonpalmer@gmail.com>
2023-04-03 11:45:29 -0700

    Set up app basic routes and import/rendered navbar component.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,29 @@
 import { useMemo } from "react"
 
-import { CssBaseline, ThemeProvider } from "@mui/material"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+import { Box, CssBaseline, ThemeProvider } from "@mui/material"
 import { createTheme } from "@mui/material/styles"
 
 import { themeSettings } from "@/theme"
+import Navbar from "@/scenes/navbar"
 
 const App = () => {
   const theme = useMemo(() => createTheme(themeSettings), [])
   return (
     <div className="app">
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-      </ThemeProvider>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Box width="100%" height="100%" padding="1rem 2rem 4rem 2rem">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<div>Dashboard Page</div>} />
+              <Route path="/predictions" element={<div>Predictions Page</div>} />
+            </Routes>
+          </Box>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   )
 }
