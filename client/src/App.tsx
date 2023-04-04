49bda2a45ecd859f3487f85c21283f78efac4417
Bryson Palmer <brysonpalmer@gmail.com>
2023-04-04 11:59:22 -0700

    Adding initial Dashboard component to App.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import { Box, CssBaseline, ThemeProvider } from '@mui/material'
 import { createTheme } from '@mui/material/styles'
 
 import { themeSettings } from '@/theme'
+import Dashboard from '@/scenes/dashboard'
 import Navbar from '@/scenes/navbar'
 
 const App = () => {
@@ -18,7 +19,7 @@ const App = () => {
           <Box width='100%' height='100%' padding='1rem 2rem 4rem 2rem'>
             <Navbar />
             <Routes>
-              <Route path='/' element={<div>Dashboard Page</div>} />
+              <Route path='/' element={<Dashboard />} />
               <Route path='/predictions' element={<div>Predictions Page</div>} />
             </Routes>
           </Box>
