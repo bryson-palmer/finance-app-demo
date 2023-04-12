339d8117afdaebd559429fd0d5301bc8ad4629be
Bryson Palmer <brysonpalmer@gmail.com>
2023-04-11 23:45:18 -0700

    Set up server with express.

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import express from 'express'
+import helmet from 'helmet'
+import mongoose from 'mongoose'
+import morgan from 'morgan'
+
+/* Configurations */
+dotenv.config()
+const app = express()
+app.use(express.json())
+app.use(helmet())
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }))
+app.use(morgan('common'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+
+/* Mongoose set up */
